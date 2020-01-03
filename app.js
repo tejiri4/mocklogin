f3707e67f9d5ba8830547d1efac08072d091962e
tejiri4 <tejirimatthew4@gmail.com>
2020-01-03 23:00:00 +0100

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 require('dotenv').config()
-
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Welcome to mock login!'))
@@ -9,8 +11,14 @@ app.get('/', (req, res) => res.send('Welcome to mock login!'))
 app.post('/login/:shouldFail', (req, res) => {
     const userExist = req.params.shouldFail === 'true' ? false : true;
     return res.status(200).json({
-        userExist
+        userExist,
+        name: "John Doe",
+        id: "13242466"
     })
 });
+
+app.all('*', (req, res) => res.status(400).json({
+    message: 'Route not found'
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
